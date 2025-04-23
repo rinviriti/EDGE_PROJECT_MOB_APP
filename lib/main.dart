a8eeeac93d3753bdad621aa6a64fef7e8c1b771b
Rinvi Jaman Riti <155306221+rinviriti@users.noreply.github.com>
2025-04-23 22:56:09 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,123 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(const MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import 'model/notifiers/bannerAd_notifier.dart';
+import 'model/notifiers/brands_notifier.dart';
+import 'model/notifiers/cart_notifier.dart';
+import 'model/notifiers/notifications_notifier.dart';
+import 'model/notifiers/orders_notifier.dart';
+import 'model/notifiers/products_notifier.dart';
+import 'model/notifiers/userData_notifier.dart';
+import 'model/services/auth_service.dart';
+import 'screens/getstarted_screens/intro_screen.dart';
+import 'screens/getstarted_screens/splash_screen.dart';
+import 'utils/colors.dart';
+import 'widgets/provider.dart';
+import 'widgets/tabsLayout.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return exit(0);
+        }
+
+        // Once complete
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyProvider(
+            auth: AuthService(),
+            child: AnnotatedRegion<SystemUiOverlayStyle>(
+              value: SystemUiOverlayStyle(
+                statusBarColor: Colors.transparent,
+                statusBarIconBrightness: Brightness.dark,
+                systemNavigationBarIconBrightness: Brightness.dark,
+              ),
+              child: HomeController(),
+            ),
+          );
+        }
+        return SplashScreen();
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeController extends StatefulWidget {
+  const HomeController({Key key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeControllerState createState() => _HomeControllerState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomeControllerState extends State<HomeController> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    final AuthService auth = MyProvider.of(context).auth;
+
+    return StreamBuilder(
+        stream: auth.onAuthStateChanged,
+        builder: (context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            final bool signedIn = snapshot.hasData;
+            return MaterialApp(
+              title: "Pet Shop",
+              theme: ThemeData(
+                accentColor: MColors.primaryPurple,
+                primaryColor: MColors.primaryPurple,
+              ),
+              debugShowCheckedModeBanner: false,
+              home: signedIn
+                  ? MultiProvider(
+                      providers: [
+                        ChangeNotifierProvider(
+                          create: (context) => ProductsNotifier(),
+                        ),
+                        ChangeNotifierProvider(
+                          create: (context) => BrandsNotifier(),
+                        ),
+                        ChangeNotifierProvider(
+                          create: (context) => CartNotifier(),
+                        ),
+                        ChangeNotifierProvider(
+                          create: (context) => UserDataProfileNotifier(),
+                        ),
+                        ChangeNotifierProvider(
+                          create: (context) => UserDataAddressNotifier(),
+                        ),
+                        ChangeNotifierProvider(
+                          create: (context) => OrderListNotifier(),
+                        ),
+                        ChangeNotifierProvider(
+                          create: (context) => NotificationsNotifier(),
+                        ),
+                        ChangeNotifierProvider(
+                          create: (context) => BannerAdNotifier(),
+                        ),
+                      ],
+                      child: TabsLayout(),
+                    )
+                  : IntroScreen(),
+            );
+          }
+          return SplashScreen();
+        });
   }
 }
